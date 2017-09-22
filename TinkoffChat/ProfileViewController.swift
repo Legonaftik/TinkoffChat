872f01f28a4415c6ccde17326c4248fe490e9d65
Vladimir Pavlov <legonaftik@gmail.com>
2017-09-22 23:50:49 +0300

    Initial commit

---
+++ TinkoffChat/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  TinkoffChat
+//
+//  Created by Vladimir Pavlov on 20/09/2017.
+//  Copyright © 2017 Vladimir Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
