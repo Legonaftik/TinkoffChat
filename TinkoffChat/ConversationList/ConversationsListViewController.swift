6a5bc6de0195c333c286cac009eb5affdf03e3dd
Vladimir Pavlov <legonaftik@gmail.com>
2017-10-08 21:11:20 +0300

    Completed ConversationsListViewController with pseudo-data

--- TinkoffChat/ConversationList/ConversationsListViewController.swift
+++ TinkoffChat/ConversationList/ConversationsListViewController.swift
@@ -29,6 +29,35 @@ extension ConversationsListViewController: UITableViewDataSource, UITableViewDel
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ConversationTableViewCell
+
+        // Filling the cell
+        // Name
+        cell.nameLabel.text = "Name:\(indexPath.section)\(indexPath.row)"
+        // Message
+        let message: String? = (indexPath.row % 2 == 0) ? "There is some message!!!" : nil
+        if message == nil {
+            cell.messageLabel.font = UIFont.italicSystemFont(ofSize: 14)
+            cell.messageLabel.text = "No message yet"
+        } else {
+            cell.messageLabel.font = UIFont.systemFont(ofSize: 17)
+            cell.messageLabel.text = message
+        }
+        // Date
+        let date = (indexPath.row % 3 == 0) ? Date() : Date(timeIntervalSince1970: 100500)
+        let dateFormatter = DateFormatter()
+        if Calendar.current.isDateInToday(date) {
+            dateFormatter.dateFormat = "HH:mm"
+        } else {
+            dateFormatter.dateFormat = "dd MMM"
+        }
+        cell.dateLabel.text = dateFormatter.string(from: date)
+        // Online
+        let online = indexPath.section == 0
+        cell.backgroundColor = online ? .yellow : .white
+        // Unread messages
+        let hasUnreadMessages = indexPath.row % 5 == 0
+        cell.messageLabel.font = hasUnreadMessages ? UIFont.boldSystemFont(ofSize: 18) : UIFont.systemFont(ofSize: 17)
+
         return cell
     }
 
