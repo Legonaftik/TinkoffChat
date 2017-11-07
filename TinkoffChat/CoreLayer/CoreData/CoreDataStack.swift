5e74c99c61390053eb97c4934136eeb1a5ea08ee
Vladimir Pavlov <legonaftik@gmail.com>
2017-11-07 17:17:26 +0300

    Implemented CoreDataStack

---
+++ TinkoffChat/CoreLayer/CoreData/CoreDataStack.swift
@@ -0,0 +1,106 @@
+//
+//  CoreDataStack.swift
+//  TinkoffChat
+//
+//  Created by Vladimir Pavlov on 07/11/2017.
+//  Copyright © 2017 Vladimir Pavlov. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+
+    private var storeURL: URL {
+        get {
+            let documentsDirURL: URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            let url = documentsDirURL.appendingPathComponent("TinkoffChat.sqlite")
+            return url
+        }
+    }
+
+    private let managedObjectModelName = "TinkoffChat"
+    private var _managedObjectModel: NSManagedObjectModel?
+    private var managedObjectModel: NSManagedObjectModel? {
+        get {
+            if _managedObjectModel == nil {
+                guard let modelURL = Bundle.main.url(forResource: managedObjectModelName, withExtension: "momd") else {
+                    fatalError("Empty model URL!")
+                }
+                _managedObjectModel = NSManagedObjectModel(contentsOf: modelURL)
+            }
+            return _managedObjectModel
+        }
+    }
+
+    private var _persistentStoreCoordinator: NSPersistentStoreCoordinator?
+    private var persistentStoreCoordinator: NSPersistentStoreCoordinator? {
+        get {
+            if _persistentStoreCoordinator == nil {
+                guard let model = self.managedObjectModel else {
+                    fatalError("Empty managed object model!")
+                }
+
+                _persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
+
+                do {
+                    try _persistentStoreCoordinator?.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+                } catch {
+                    fatalError("Error adding persistent store to coordinator: \(error.localizedDescription)")
+                }
+            }
+            return _persistentStoreCoordinator
+        }
+    }
+
+    private var _masterContext: NSManagedObjectContext?
+    private var masterContext: NSManagedObjectContext? {
+        get {
+            if _masterContext == nil {
+                let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+                guard let persistentStoreCoordinator = persistentStoreCoordinator else {
+                    fatalError("Empty persistent store coordinator!")
+                }
+                context.persistentStoreCoordinator = persistentStoreCoordinator
+                context.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
+                context.undoManager = nil
+                _masterContext = context
+            }
+            return _masterContext
+        }
+    }
+
+    private var _mainContext: NSManagedObjectContext?
+    public var mainContext: NSManagedObjectContext? {
+        get {
+            if _mainContext == nil {
+                let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+                guard let parentContext = masterContext else {
+                    fatalError("No master context!")
+                }
+                context.parent = parentContext
+                context.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
+                context.undoManager = nil
+                _mainContext = context
+            }
+            return _mainContext
+        }
+    }
+
+    private var _saveContext: NSManagedObjectContext?
+    public var saveContext: NSManagedObjectContext? {
+        get {
+            if _saveContext == nil {
+                let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+                guard let parentContext = mainContext else {
+                    fatalError("No main context!")
+                }
+                context.parent = parentContext
+                context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+                context.undoManager = nil
+                _saveContext = context
+            }
+            return _saveContext
+        }
+    }
+}
